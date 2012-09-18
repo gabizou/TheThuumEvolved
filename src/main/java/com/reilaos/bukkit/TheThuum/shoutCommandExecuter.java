a535ae549801537fb2887dacb9a4d6b6206e1827
Rogue <JD_Becker@comcast.net>
2012-09-18 00:01:16 -0500

    Changed switch statement in command executer for java 1.6 compatibility

--- src/main/java/com/reilaos/bukkit/TheThuum/shoutCommandExecuter.java
+++ src/main/java/com/reilaos/bukkit/TheThuum/shoutCommandExecuter.java
@@ -29,14 +29,14 @@ public class shoutCommandExecuter implements CommandExecutor
     	}
     	if(player != null)
     	{
-    		switch(command.getName().toLowerCase())
-    		{
-    			case "fus":
+    			if(command.getName().equalsIgnoreCase("fus"))
+    			{
     				if(args.length == 2)
     				{
     					if(args[0].equalsIgnoreCase("Ro") && args[1].equalsIgnoreCase("Dah"))
     					{
     						shout(player, ShoutType.FUSRODAH, 3);
+    						return true;
     					}
     				}
     				else if(args.length == 1)
@@ -44,24 +44,29 @@ public class shoutCommandExecuter implements CommandExecutor
     					if(args[0].equalsIgnoreCase("Ro"))
     					{
     						shout(player, ShoutType.FUSRODAH, 2);
+    						return true;
     					}
     				}
     				else if(args.length == 0)
     				{
     					shout(player, ShoutType.FUSRODAH, 1);
+    					return true;
     				}
     				else
     				{
     					//probably more arguments then actually exist
     					player.sendMessage(ChatColor.RED + "You shouted too many words.");
+    					return true;
     				}
-    				break;
-    			case "feim":
+    			}
+    			else if(command.getName().equalsIgnoreCase("feim"))
+    			{
     				if(args.length == 2)
     				{
     					if(args[0].equalsIgnoreCase("Zii") && args[1].equalsIgnoreCase("Gron"))
     					{
     						shout(player, ShoutType.FEIMZIIGRON, 3);
+    						return true;
     					}
     				}
     				else if(args.length == 1)
@@ -69,24 +74,29 @@ public class shoutCommandExecuter implements CommandExecutor
     					if(args[0].equalsIgnoreCase("Zii"))
     					{
     						shout(player, ShoutType.FEIMZIIGRON, 2);
+    						return true;
     					}
     				}
     				else if(args.length == 0)
     				{
     					shout(player, ShoutType.FEIMZIIGRON, 1);
+    					return true;
     				}
     				else
     				{
     					//probably more arguments then actually exist
     					player.sendMessage(ChatColor.RED + "You shouted too many words.");
+    					return true;
     				}
-    				break;
-    			case "kaan":
+    			}
+    			else if(command.getName().equalsIgnoreCase("kaan"))
+    			{
     				if(args.length == 2)
     				{
     					if(args[0].equalsIgnoreCase("Drem") && args[1].equalsIgnoreCase("Ov"))
     					{
     						shout(player, ShoutType.KAANDREMOV, 3);
+    						return true;
     					}
     				}
     				else if(args.length == 1)
@@ -94,24 +104,29 @@ public class shoutCommandExecuter implements CommandExecutor
     					if(args[0].equalsIgnoreCase("Drem"))
     					{
     						shout(player, ShoutType.KAANDREMOV, 2);
+    						return true;
     					}
     				}
     				else if(args.length == 0)
     				{
     					shout(player, ShoutType.KAANDREMOV, 1);
+    					return true;
     				}
     				else
     				{
     					//probably more arguments then actually exist
     					player.sendMessage(ChatColor.RED + "You shouted too many words.");
+    					return true;
     				}
-    				break;
-    			case "laas":
+    			}
+    			else if(command.getName().equalsIgnoreCase("laas"))
+    			{
     				if(args.length == 2)
     				{
     					if(args[0].equalsIgnoreCase("Yah") && args[1].equalsIgnoreCase("Nir"))
     					{
     						shout(player, ShoutType.LAASYAHNIR, 3);
+    						return true;
     					}
     				}
     				else if(args.length == 1)
@@ -119,24 +134,29 @@ public class shoutCommandExecuter implements CommandExecutor
     					if(args[0].equalsIgnoreCase("Yah"))
     					{
     						shout(player, ShoutType.LAASYAHNIR, 2);
+    						return true;
     					}
     				}
     				else if(args.length == 0)
     				{
     					shout(player, ShoutType.LAASYAHNIR, 1);
+    					return true;
     				}
     				else
     				{
     					//probably more arguments then actually exist
     					player.sendMessage(ChatColor.RED + "You shouted too many words.");
+    					return true;
     				}
-    				break;
-    			case "lok":
+    			}
+    			else if(command.getName().equalsIgnoreCase("lok"))
+    			{
     				if(args.length == 2)
     				{
     					if(args[0].equalsIgnoreCase("Vah") && args[1].equalsIgnoreCase("Koor"))
     					{
     						shout(player, ShoutType.LOKVAHKOOR, 3);
+    						return true;
     					}
     				}
     				else if(args.length == 1)
@@ -144,24 +164,29 @@ public class shoutCommandExecuter implements CommandExecutor
     					if(args[0].equalsIgnoreCase("Vah"))
     					{
     						shout(player, ShoutType.LOKVAHKOOR, 2);
+    						return true;
     					}
     				}
     				else if(args.length == 0)
     				{
     					shout(player, ShoutType.LOKVAHKOOR, 1);
+    					return true;
     				}
     				else
     				{
     					//probably more arguments then actually exist
     					player.sendMessage(ChatColor.RED + "You shouted too many words.");
+    					return true;
     				}
-    				break;
-    			case "wuld":
+    			}
+    			else if(command.getName().equalsIgnoreCase("wuld"))
+    			{
     				if(args.length == 2)
     				{
     					if(args[0].equalsIgnoreCase("Nah") && args[1].equalsIgnoreCase("Kest"))
     					{
     						shout(player, ShoutType.WULDNAHKEST, 3);
+    						return true;
     					}
     				}
     				else if(args.length == 1)
@@ -169,24 +194,29 @@ public class shoutCommandExecuter implements CommandExecutor
     					if(args[0].equalsIgnoreCase("Nah"))
     					{
     						shout(player, ShoutType.WULDNAHKEST, 2);
+    						return true;
     					}
     				}
     				else if(args.length == 0)
     				{
     					shout(player, ShoutType.WULDNAHKEST, 1);
+    					return true;
     				}
     				else
     				{
     					//probably more arguments then actually exist
     					player.sendMessage(ChatColor.RED + "You shouted too many words or gibberish.");
+    					return true;
     				}
-    				break;
-    			case "yol":
+    			}
+    			else if(command.getName().equalsIgnoreCase("yol"))
+    			{
     				if(args.length == 2)
     				{
     					if(args[0].equalsIgnoreCase("Toor") && args[1].equalsIgnoreCase("Shul"))
     					{
     						shout(player, ShoutType.YOLTOORSHUL, 3);
+    						return true;
     					}
     				}
     				else if(args.length == 1)
@@ -194,20 +224,22 @@ public class shoutCommandExecuter implements CommandExecutor
     					if(args[0].equalsIgnoreCase("Toor"))
     					{
     						shout(player, ShoutType.YOLTOORSHUL, 2);
+    						return true;
     					}
     				}
     				else if(args.length == 0)
     				{
     					shout(player, ShoutType.YOLTOORSHUL, 1);
+    					return true;
     				}
     				else
     				{
     					//probably more arguments then actually exist
     					player.sendMessage(ChatColor.RED + "You shouted too many words.");
+    					return true;
     				}
-    				break;
+    			}
     		}
-    	}
     	
     	return false;
 	}
